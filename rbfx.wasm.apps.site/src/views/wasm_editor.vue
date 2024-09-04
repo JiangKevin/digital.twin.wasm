57ee4db863e1b890452c173286df92b6cf8ad9bf
JiangKevin <JiangKevin@users.noreply.github.com>
2024-09-04 17:10:58 +0800

    备份一下

--- rbfx.wasm.apps.site/src/views/wasm_editor.vue
+++ rbfx.wasm.apps.site/src/views/wasm_editor.vue
@@ -2,16 +2,7 @@
     <div class="main_container">
         <div class="main_container_toolbar">
             <v-divider vertical class="divider_vertical"></v-divider>
-            <button class="toolbar_btn" @click="code_ck">
-                <i class="mdi-crowd mdi"></i
-                ><v-tooltip
-                    activator="parent"
-                    content-class="toolbar_btn_tooltip"
-                    opacity="0.1"
-                    location="end"
-                    >Open Digital Twin Code Editor</v-tooltip
-                >
-            </button>
+            <button class="toolbar_btn" @click="code_ck"><i class="mdi-crowd mdi"></i><v-tooltip activator="parent" content-class="toolbar_btn_tooltip" opacity="0.1" location="end">Open Digital Twin Code Editor</v-tooltip></button>
             <v-divider vertical class="divider_vertical"></v-divider>
         </div>
         <div class="main_container_content">
@@ -24,43 +15,46 @@
 </template>
 <!--  -->
 <script setup>
-    import { ref, onMounted, onUnmounted, onUpdated, onActivated } from "vue";
-    import { useStoreForMenu } from "@/stores/globle.js";
-    const mainStore_menu = useStoreForMenu();
-    //
-    import { fm_addScript, simple_wasm } from "@/plugins/base.js";
-    //
-    onMounted(() => {
-        if (!is_load_rbfx_wasm) {
-            Module = {
-                preRun: [],
-                postRun: [],
-                print: (function () {
-                    return function (text) {
-                        console.log("+- From c++: " + text);
-                    };
-                })(),
-                canvas: document.getElementById("canvas"),
-            };
-            //
-            Module["preRun"].push(function () {
-                Module["addRunDependency"]("IndexedDB");
-                FS.mkdir("/IndexedDB");
-                // FS.mount(IDBFS, {}, "/IndexedDB");
-                FS.syncfs(true, function (err) {
-                    if (err) {
-                        console.error(err);
-                    }
-                    Module["removeRunDependency"]("IndexedDB");
-                });
+import { ref, onMounted, onUnmounted, onUpdated, onActivated } from "vue";
+import { useStoreForMenu } from "@/stores/globle.js";
+const mainStore_menu = useStoreForMenu();
+//
+import { fm_addScript, simple_wasm } from "@/plugins/base.js";
+//
+onMounted(() => {
+    console.log("+- From js: is_load_rbfx_wasm = " + is_load_rbfx_wasm);
+    if (!is_load_rbfx_wasm) {
+        Module = {
+            preRun: [],
+            postRun: [],
+            print: (function () {
+                return function (text) {
+                    console.log("+- From c++: " + text);
+                };
+            })(),
+            canvas: document.getElementById("canvas"),
+        };
+        //
+        Module["preRun"].push(function () {
+            Module["addRunDependency"]("IndexedDB");
+            FS.mkdir("/IndexedDB");
+            // FS.mount(IDBFS, {}, "/IndexedDB");
+            FS.syncfs(true, function (err) {
+                if (err) {
+                    console.error(err);
+                }
+                Module["removeRunDependency"]("IndexedDB");
             });
-            //
-            fm_addScript("./data.js", true);
-            fm_addScript("./common.js", true);
-        }
-    });
-    //
-    function code_ck() {}
+        });
+        //
+        fm_addScript("./data.js", true);
+        fm_addScript("./common.js", true);
+        //
+        is_load_rbfx_wasm = true;
+    }
+});
+//
+function code_ck() {}
 </script>
 <!--  -->
 <style scoped>

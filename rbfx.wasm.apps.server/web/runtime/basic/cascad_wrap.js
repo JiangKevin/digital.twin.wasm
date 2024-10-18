91c9873ff419777c2edc7e6d497b52ab8ed14c05
JiangKevin <JiangKevin@users.noreply.github.com>
2024-10-18 10:10:01 +0800

    备份一下

--- rbfx.wasm.apps.server/web/runtime/basic/cascad_wrap.js
+++ rbfx.wasm.apps.server/web/runtime/basic/cascad_wrap.js
@@ -279,7 +279,32 @@ FMCAS_.makeBottle = function (openCascade, myWidth, myHeight, myThickness) {
 
     return aRes;
 };
+//
+FMCAS_.visualizeDoc = function (oc, doc) {
+    // Export a GLB file (this will also perform the meshing)
+    const cafWriter = new oc.RWGltf_CafWriter(new oc.TCollection_AsciiString_2("./file.glb"), true);
+    cafWriter.Perform_2(new oc.Handle_TDocStd_Document_2(doc), new oc.TColStd_IndexedDataMapOfStringString_1(), new oc.Message_ProgressRange_1());
 
+    // Read the GLB file from the virtual file system
+    const glbFile = oc.FS.readFile("./file.glb", { encoding: "binary" });
+    return URL.createObjectURL(new Blob([glbFile.buffer], { type: "model/gltf-binary" }));
+};
+//
+FMCAS_.addVisualizeShapesToView = function (oc, shapes_) {
+    const shapes = Array.isArray(shapes_) ? shapes_ : [shapes_];
+
+    // Create a document add our shapes
+    const doc = new oc.TDocStd_Document(new oc.TCollection_ExtendedString_1());
+    const shapeTool = oc.XCAFDoc_DocumentTool.ShapeTool(doc.Main()).get();
+    for (const s of shapes) {
+        shapeTool.SetShape(shapeTool.NewShape(), s);
+        // Tell OpenCascade that we want our shape to get meshed
+        new oc.BRepMesh_IncrementalMesh_2(s, 0.1, false, 0.1, false);
+    }
+
+    // Return our visualized document
+    return visualizeDoc(oc, doc);
+};
 //
 const fm_cascad_wrap_load_ok_event = new Event("CASCAD_WRAP_LOADED");
 window.dispatchEvent(fm_cascad_wrap_load_ok_event);

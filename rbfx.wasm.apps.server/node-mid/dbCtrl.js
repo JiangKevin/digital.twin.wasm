e226449c0a05abd0a6cd4622ec0ea498c4892f84
JiangKevin <JiangKevin@users.noreply.github.com>
2024-11-28 12:24:58 +0800

    备份一下

--- rbfx.wasm.apps.server/node-mid/dbCtrl.js
+++ rbfx.wasm.apps.server/node-mid/dbCtrl.js
@@ -1,6 +1,7 @@
 const promise = require("bluebird");
 const pgp = require("pg-promise")({ promiseLib: promise });
 const db = pgp("postgres://postgres:kevin@192.168.1.102:5432/SpaceDb");
+const fs = require("fs");
 //////////////////////////////////////
 //
 //
@@ -12,16 +13,23 @@ function select_osm_polygon_of_building(req, res) {
   var lat = req.query.lat;
   var lon = req.query.lon;
   var sql =
-    "SELECT t.osm_id,t.name, ST_AsText(t.way) FROM public.planet_osm_polygon t where ST_DWithin(ST_GeographyFromText('SRID=4326;POINT(" +
+    "SELECT t.osm_id,t.name, ST_AsText(t.way) as geom FROM public.planet_osm_polygon t where ST_DWithin(ST_GeographyFromText('SRID=4326;POINT(" +
     lon +
     " " +
     lat +
     ")'),ST_Transform(t.way,4326),100);";
+  //
+  //   console.log(sql);
+  var json_name = "./tmp/" + req.query.lon + "-" + req.query.lat + ".json";
+
   // 读取记录
   db.any(sql)
     .then((records) => {
+      fs.writeFileSync(json_name, JSON.stringify(records));
+      //
       records.forEach((record) => {
-        console.log(record);
+        // console.log(record);
+        // fs.writeFileSync(json_name, JSON.stringify(record));
       });
     })
     .catch((error) => {
